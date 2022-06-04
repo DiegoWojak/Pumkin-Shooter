9b0dfb0128d120a61773632c7a8392c897d54246
Diego <diego_14182_5@hotmail.com>
2022-06-04 02:10:58 -0500

    Data I/O

---
+++ Assets/Scripts/com/DA/DataObject.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Assets.Scripts.com.DA
+{
+    [CreateAssetMenu(menuName = "Data/AccessDataObject")]
+    public class DataObject : ScriptableObject
+    {
+        public string Name => _name;
+        [SerializeField] private string _name; 
+
+        [ContextMenu("Save")]
+        public void SaveState()
+        {
+            var json = JsonUtility.ToJson(this);
+            File.WriteAllText(GetFilePath(), json);
+            Debug.Log(GetFilePath());
+        }
+
+        [ContextMenu("Load")]
+        public void LoadState()
+        {
+            var json = File.ReadAllText(GetFilePath());
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+
+        private string GetFilePath()
+        {
+            return (Application.persistentDataPath + $"/{Name}.so");
+        }
+    }
+}
